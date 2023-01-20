5e94ce217dfa62fcd4ac907f091c1effd38666c2
Joseph Lorenzini <jaloren@gmail.com>
2023-01-19 20:15:05 -0600

    initial refactor of transaction

--- database/database.go
+++ database/database.go
@@ -28,140 +28,34 @@ var (
 )
 
 type Database struct {
-	data   map[string]*string
-	valCnt map[string]int
-	trans  *transaction
+	trans *transaction
 }
 
 func New() *Database {
 	return &Database{
-		data:   make(map[string]*string),
-		valCnt: make(map[string]int),
+		trans: nil,
 	}
 }
 
 func (d *Database) Set(name, value string) {
-	if d.trans != nil {
-		d.trans.add(d, name, ptr(value))
-		return
-	}
-	if existing, ok := d.data[name]; ok && existing != nil {
-		// there's an existing name, but the value is being updated so decrement the old value counter
-		// and increment the new value counter
-		d.decreValCnt(deref(existing))
-	} else if ok && deref(existing) == value {
-		// to avoid double counting duplicate puts return early
-		return
-	}
-	d.increValCnt(value)
-	d.data[name] = ptr(value)
 }
 
 func (d *Database) Get(name string) string {
-	if d.trans != nil {
-		currentTransaction := d.trans
-		val, ok := currentTransaction.get(name)
-		if ok {
-			return val
-		}
-	}
-	val, ok := d.data[name]
-	if !ok || val == nil {
-		return nullValue
-	}
-	return deref(val)
 }
 
 func (d *Database) Delete(name string) {
-	if d.trans != nil {
-		d.trans.add(d, name, nil)
-		return
-	}
-	if existing, ok := d.data[name]; ok && existing == nil {
-		// to avoid double counting duplicate delete return early
-		return
-	} else if ok {
-		d.decreValCnt(deref(existing))
-	}
-	d.data[name] = nil
 }
 
 func (d *Database) Count(value string) int {
-	if d.trans == nil {
-		totalCnt, _ := d.valCnt[value]
-		return totalCnt
-	}
-	currentTrans := d.trans
-	for {
-		for transVal, transValCnt := range currentTrans.valCnt {
-			if transVal != value {
-				continue
-			}
-			return transValCnt
-		}
-		if currentTrans.prev == nil {
-			break
-		}
-		currentTrans = currentTrans.prev
-	}
-	return 0
 }
 
 func (d *Database) BeginTransaction() {
-	if d.trans == nil {
-		d.trans = newTransaction(nil)
-	} else {
-		d.trans = newTransaction(d.trans)
-	}
 }
 
 func (d *Database) CommitTransactions() {
-	currentTrans := d.trans
-	for {
-		for name, value := range currentTrans.data {
-			d.data[name] = value
-			for valNames, cnt := range currentTrans.valCnt {
-				if origCnt, ok := d.valCnt[valNames]; ok {
-					d.valCnt[valNames] = origCnt + cnt
-				} else {
-					d.valCnt[valNames] = cnt
-				}
-			}
-		}
-		if currentTrans.prev == nil {
-			break
-		}
-		currentTrans = currentTrans.prev
-	}
-	d.trans = nil
 }
 
 func (d *Database) RollbackLastTransaction() error {
-	if d.trans == nil {
-		return TransactionNotFoundErr
-	}
-	if d.trans.prev == nil {
-		d.trans = nil
-		return nil
-	}
-	d.trans = d.trans.prev
-	return nil
-}
-
-func (d *Database) increValCnt(value string) {
-	cnt, ok := d.valCnt[value]
-	if ok {
-		d.valCnt[value] = cnt + 1
-	} else {
-		d.valCnt[value] = 1
-	}
-}
-
-func (d *Database) decreValCnt(value string) {
-	cnt, ok := d.valCnt[value]
-	if ok && cnt > 0 {
-		d.valCnt[value] = cnt - 1
-	}
 }
 
 func (d *Database) ExecStatement(text string) (string, error) {
--- database/transaction.go
+++ database/transaction.go
@@ -1,134 +1,82 @@
 package database
 
+import "golang.org/x/exp/maps"
+
+type set map[string]struct{}
+
 type transaction struct {
-	valCnt map[string]int
-	data   map[string]*string
-	prev   *transaction
+	delNames     set
+	nameToValue  map[string]string
+	valueToNames map[string]set
+	prev         *transaction
 }
 
-func newTransaction(prev *transaction) *transaction {
-	return &transaction{
-		valCnt: make(map[string]int),
-		data:   make(map[string]*string),
-		prev:   prev,
-	}
-}
-
-func (t *transaction) get(name string) (string, bool) {
-	val, ok := t.data[name]
-	if !ok {
-		return "", false
-	}
-	if val != nil {
-		return deref(val), true
-	}
-	return nullValue, true
-}
-
-// SET a foo
-// BEGIN
-// SET b foo
-// DELETE a
-// BEGIN
-// SET c foo
-
-// BEGIN
-// SET a foo
-
-func (t *transaction) add(db *Database, name string, rawValue *string) {
-	t.data[name] = rawValue
-	current := t
-
-	var deletedRawValue *string
-	for {
-		existingRawVal, ok := current.data[name]
-		if !ok {
-			current = t.prev
-			if current == nil {
-				break
-			}
-			continue
-		}
-		if deletedRawValue == nil && existingRawVal != nil {
-			deletedRawValue = existingRawVal
-		}
-		currentCnt, ok := current.valCnt[deref(existingRawVal)]
-		if !ok {
-			current = t.prev
-			if current == nil {
-				break
-			}
-			continue
-		}
-		if rawValue == nil {
-			if currentCnt > 0 {
-				t.valCnt[deref(existingRawVal)] = currentCnt - 1
-			}
-
-			return
-		} else if rawValue != nil {
-			t.valCnt[deref(rawValue)] = currentCnt + 1
-			return
-		} else {
-			current = t.prev
-			if current == nil {
-				break
-			}
-		}
-	}
-	if deletedRawValue == nil {
-		deletedRawValue = db.data[name]
-	}
-
-	if currentCnt, ok := db.valCnt[deref(deletedRawValue)]; ok {
-		if rawValue == nil {
-			if currentCnt > 0 {
-				t.valCnt[deref(deletedRawValue)] = currentCnt - 1
-			}
-
-			return
-		} else {
-			t.valCnt[deref(rawValue)] = currentCnt + 1
-			return
-		}
-	}
-
-	if rawValue != nil {
-		t.valCnt[deref(rawValue)] = 1
-	}
-}
-
-func transValuesAreEqual(first, second *string) bool {
-	if first == nil && second == nil {
-		return true
-	}
-	return deref(first) == deref(second)
-}
-
-func (t *transaction) increValCnt(rawValue *string) {
-	value := deref(rawValue)
-	cnt, ok := t.valCnt[value]
-	if ok {
-		t.valCnt[value] = cnt + 1
-	} else {
-		t.valCnt[value] = 1
-	}
-}
-
-func (t *transaction) decreValCnt(rawVal *string) {
-	val := deref(rawVal)
-	cnt, ok := t.valCnt[val]
-	if ok {
-		t.valCnt[val] = cnt - 1
-	}
-}
-
-func ptr(input string) *string {
-	return &input
-}
-func deref(input *string) string {
-	if input == nil {
+func (t *transaction) get(name string) string {
+	if _, ok := t.delNames[name]; ok {
 		return nullValue
 	}
-	return *input
+	if updateVal, ok := t.nameToValue[name]; ok {
+		return updateVal
+	}
+	if t.prev != nil {
+		return t.prev.get(name)
+	}
+	return nullValue
+}
+
+func (t *transaction) set(name, value string) {
+	if t.prev != nil && t.prev.get(name) == value {
+		return
+	}
+	delete(t.delNames, name)
+	t.nameToValue[name] = value
+	if names, ok := t.valueToNames[value]; ok {
+		names[name] = struct{}{}
+		t.valueToNames[value] = names
+	} else {
+		t.valueToNames[value] = set{
+			name: {},
+		}
+	}
+}
+
+func (t *transaction) delete(name string) {
+	t.delNames[name] = struct{}{}
+	delete(t.nameToValue, name)
+	val, ok := t.nameToValue[name]
+	if !ok {
+		return
+	}
+	names, ok := t.valueToNames[val]
+	if !ok {
+		return
+	}
+	for k := range names {
+		if k == name {
+			delete(names, k)
+		}
+	}
+}
+
+func (t *transaction) count(value string) int {
+	return len(t.names(value))
+}
+
+func (t *transaction) names(value string) set {
+	allNames := make(set)
+	if t.prev != nil {
+		allNames = t.prev.names(value)
+	}
+	for name := range allNames {
+		if _, ok := t.nameToValue[name]; ok {
+			delete(allNames, name)
+		}
+		if _, ok := t.delNames[name]; ok {
+			delete(allNames, name)
+		}
+	}
+	if names, ok := t.valueToNames[value]; ok {
+		maps.Copy(allNames, names)
+	}
+	return allNames
 }
